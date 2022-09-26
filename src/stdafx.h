780a5e6c90b481f8e19f5ddc8e2c5cc63c224ffe
lauthrul <lauthrul@foxmail.com>
2022-09-26 14:02:40 +0800

    Remove dumplicated defines

--- src/stdafx.h
+++ src/stdafx.h
@@ -1,6 +1,3 @@
 #pragma once
 
-#define LOG_DEBUG(fmt, ...)     _tprintf(fmt, ## __VA_ARGS__)
-#define LOG_INFO(fmt, ...)      _tprintf(fmt, ## __VA_ARGS__)
-#define LOG_WARN(fmt, ...)      _tprintf(fmt, ## __VA_ARGS__)
-#define LOG_ERROR(fmt, ...)     _tprintf(fmt, ## __VA_ARGS__)
+#include "util.h"
